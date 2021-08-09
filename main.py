578eb351d2896b54e38259415f084b65bf9c4eb7
USER@DESKTOP-BQV1LSH <ziza595@gmail.com>
2021-08-09 20:11:29 +0000

    New commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 
 
 app = Flask(__name__)
-soup = BeautifulSoup(html_source, 'html.parser')
+
 
 
 @app.route('/')
@@ -16,7 +16,19 @@ def index():
     machine_learning_response = requests.get(machine_learning_url)
     analysis_content = analysis_response.text
     machine_learning_content = machine_learning_response.text
-    return render_template('index.html')
+    soup_analysis = BeautifulSoup(analysis_content, 'html.parser')
+    soup_machine_learning = BeautifulSoup(machine_learning_content, 'html.parser')
+    return render_template(
+        'index.html',
+        analysis_url=analysis_url,
+        machine_learning_url=machine_learning_url,
+        analysis_response=analysis_response,
+        machine_learning_response=machine_learning_response,
+        analysis_content=analysis_content
+        machine_learning_content=machine_learning_content
+        soup_analysis=soup_analysis
+        soup_machine_learning=soup_machine_learning
+    )
 
 
 if __name__ == '__main__':
